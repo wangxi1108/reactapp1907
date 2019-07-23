137862eaab1dc1731b79d5cb4f98059614662f56
wwj <2532258232@qq.com>
2019-07-23 15:53:14 +0800

    11

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import './login.less'
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button } from 'antd';
 
 
 // export default class Login extends Component{
   class Login extends Component{
    
     handleSubmit = e => {
-      const form = this.props.form
-      const formobj = form.getFieldsValue()
-      console.log(111,formobj)
-
       e.preventDefault();
+
+      const form = this.props.form
+      // const formobj = form.getFieldsValue()
+      form.validateFields((err, values) => {
+        console.log('validateFields-', err,22,values);
+        if (!err) {
+            console.log('没有错可以提交', values);
+        } else {
+          
+          }
+        })
     };
+    //对密码进行自定义验证
+    validatorPwd = (rule, value, callback) => {
+      console.log('validatorPwd', value)
+      if (!value) {
+        callback('请输入密码！')
+      }
+      if (value.length<4 || value.length>12) {
+        callback('密码长度在4~12位')        
+      }
+      if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+        callback('密码必须是英文、数字或下划线组成')                
+      }
+      callback()
+    }
 
     render () {
     //写在render里面，得到强大的form对象
@@ -39,7 +60,9 @@ import { Form, Icon, Input, Button, Checkbox } from 'antd';
             </Form.Item>
             <Form.Item>
               {getFieldDecorator('password', {
-                rules: [{ required: true, message: 'Please input your Password!' }],
+                rules: [
+                  {validator:this.validatorPwd}
+                ]
               })(
                 <Input
                   prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
