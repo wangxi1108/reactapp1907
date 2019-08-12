33bfb66dd80945e02b0d5e4fb62017ae3590742a
wwj <2532258232@qq.com>
2019-08-12 17:27:31 +0800

    品类列表

--- src/pages/catrgory/category.js
+++ src/pages/catrgory/category.js
@@ -1,10 +1,71 @@
 import React, { Component } from 'react'
+import { Button,Icon,Table, Divider, Tag ,Card } from 'antd';
+import './category.less'
+
+
 //品类管理
 export default class Category extends Component{
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  
   render () {
+    const title = '一级分类列表'
+    const extra = (
+      <Button className="add-btn" type="primary"><Icon type="plus" />添加</Button>
+    )
+    const data = [
+      {
+        key: '1',
+        name: '电视'
+      },
+      {
+        key: '2',
+        name: '菜刀'
+      },
+      {
+        key: '3',
+        name: '键盘'
+      },
+    ];
+    const columns = [
+      {
+        title: '分类名称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        width: 200,
+        render: (text, record) => (
+          <span>
+            <a className="marginR10" href="javascript:;">修改分类</a>
+            <a className="marginR10" href="javascript:;">查看子项</a>
+            <a className="marginR10" href="javascript:;">删除</a>
+          </span>
+        ),
+      },
+    ];
     return (
       <div className="category">
-        category
+        {/* <div className="title-box">
+          <span className="title">一级分类列表</span>
+          <Button className="add-btn" type="primary"><Icon type="plus" />添加</Button>
+        </div>
+        <div className="table-box">
+          <Table columns={columns} dataSource={data} />
+        </div> */}
+        {/* 用card */}
+        <Card title={title} extra={extra} style={{ width: '100%' }}>
+          <div className="table-box">
+            <Table bordered columns={columns} dataSource={data} />
+          </div> 
+        </Card>
       </div>
     )
   }
---
+++ src/pages/catrgory/components/edit.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Modal } from 'antd';
+
+
+export default class editCategory extends Component {
+  
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+    );
+  }
+}
