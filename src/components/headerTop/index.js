297a8e3743460c4af83701457a6ffde3fa08681b
wwj <2532258232@qq.com>
2019-09-21 07:50:17 +0800

    像链接的按钮组件

--- src/components/headerTop/index.js
+++ src/components/headerTop/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Menu, Dropdown, Icon } from 'antd'
+import { Menu, Dropdown, Icon, Modal } from 'antd'
 import { withRouter } from 'react-router-dom';
+import LinkButton from '../../components/linkButton'
 import './index.less'
 import memory from '../../utils/memory'
 import storage from '../../utils/storage'
@@ -32,8 +33,15 @@ class HeaderTop extends Component{
     },1000*360*24)
   }
   loginOut = () => {
-    storage.removeUser();
-    this.props.history.push('/')
+    Modal.confirm({
+      title: '确定退出？',
+      onOk:()=> {
+        storage.removeUser();
+        memory.user={}
+        this.props.history.push('/')
+      }
+    });
+    
   }
   componentDidMount(){
     this.getTime()
@@ -44,11 +52,12 @@ class HeaderTop extends Component{
       <div className="headerTop">
         HeaderTop 欢迎：{memory.user.username}
         {currentTime}
-        <span className="login-out" onClick={()=>this.loginOut()}>退出</span>
+        {/* <span className="login-out" onClick={()=>this.loginOut()}>退出</span> */}
+        <LinkButton onClick={()=>this.loginOut()}>退出</LinkButton>
 
         <div className="user-ul">
           <Dropdown overlay={menu}>
-            <a className="ant-dropdown-link" href="#">
+            <a className="ant-dropdown-link" href="javascript:">
               {memory.user.username}
               <Icon type="down" />
             </a>
